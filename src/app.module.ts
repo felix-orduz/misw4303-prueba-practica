4a6f11cb8779503f9ff971b0c4b4c8ae07d05ab0
Felix Orduz <f.orduz@uniandes.edu.co>
2025-05-17 00:16:12 -0500

    chore: remove .gitignore and eslint.config.mjs files to clean up project structure feat(package.json): update project name to 'backend' and upgrade dependencies for better performance and security feat(app.module.ts): configure TypeORM with PostgreSQL and add new modules for Restaurante and Plato fix(app.service.ts): update greeting message to reflect project context feat(main.ts): enable CORS, API versioning, global validation pipe, and Swagger documentation for better API management fix(tsconfig.json): downgrade TypeScript target to ES2021 and adjust strictness settings for compatibility

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RestauranteModule } from './restaurante/restaurante.module';
+import { PlatoModule } from './plato/plato.module';
+import { RestaurantePlatoModule } from './restaurante-plato/restaurante-plato.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RestauranteEntity } from './restaurante/restaurante.entity';
+import { PlatoEntity } from './plato/plato.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '102261',
+      database: 'restaurante',
+      entities: [
+        RestauranteEntity,
+        PlatoEntity,
+      ],
+      dropSchema: false,
+      synchronize: true,
+    }),
+    RestauranteModule,
+    PlatoModule,
+    RestaurantePlatoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Enable CORS
+  app.enableCors();
+
+  // Enable API versioning
+  app.enableVersioning({
+    type: VersioningType.URI,
+    prefix: 'api/v',
+    defaultVersion: '1',
+  });
+
+  // Global validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Swagger documentation
+  const config = new DocumentBuilder()
+    .setTitle('Prueba Practica Restaurantes')
+    .setDescription('API para la gestión de restaurantes')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
